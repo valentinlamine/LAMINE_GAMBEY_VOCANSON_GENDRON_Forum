1f890c3aa64551cb0533255ad7129abd2bfde7a4
AdrienGendron <112941884+AdrienGend@users.noreply.github.com>
2023-06-05 17:10:24 +0200

    Header and filetree

---
+++ src/JS/header.js
@@ -0,0 +1,12 @@
+document.getElementById('search-button').addEventListener('click', function() {
+    var searchTerm = document.getElementById('search-input').value;
+    alert('Recherche pour: ' + searchTerm);
+});
+
+document.getElementById('login-button').addEventListener('click', function() {
+    alert('Fonction de connexion non implémentée');
+});
+
+document.getElementById('signup-button').addEventListener('click', function() {
+    alert('Fonction d\'inscription non implémentée');
+});
