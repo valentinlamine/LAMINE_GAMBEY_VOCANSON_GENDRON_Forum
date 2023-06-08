2d36191bc9de83b4197299bf43ee44a5e3dd475b
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2023-06-08 16:33:27 +0200

    update

--- src/JS/format.js
+++ src/JS/format.js
@@ -1,7 +1,7 @@
 
 var i = 0;
 function formatNumber(number) {
-    const suffixes = ["", "k", "M", "MD"]; 
+    const suffixes = ["", " k", " m", " b"]; 
     const magnitude = Math.floor(Math.log10(number) / 3); 
     
     const scaledNumber = number / Math.pow(1000, magnitude); 
@@ -9,9 +9,29 @@ function formatNumber(number) {
     
     const view = document.getElementsByClassName("view"); 
     let result = formattedNumber + suffixes[magnitude];
-    view[i].innerHTML = result;
+    //if result contain a .0 dot remove it
+    if (result.includes(".0")) {
+        result = result.replace(".0", "");
+    }
+
+
+
+
+    view[i].innerHTML = result+" views";
     i++;
   }
+
+
+
+
+
+
+
+
+
+
+
+
 
   function calculateDaysSince(dateString) {
     var currentDate = new Date();
@@ -19,12 +39,10 @@ function formatNumber(number) {
     console.log(currentDate);
     console.log(targetDate);
   
-    // Convertir currentDate en 000Z (heure à 00:00:00)
     currentDate.setHours(0);
     currentDate.setMinutes(0);
     currentDate.setSeconds(0);
   
-    // Définir l'heure, les minutes et les secondes de la date cible avec la date actuelle
     targetDate.setHours(currentDate.getHours());
     targetDate.setMinutes(currentDate.getMinutes());
     targetDate.setSeconds(currentDate.getSeconds());
@@ -33,20 +51,16 @@ function formatNumber(number) {
   
     if (timeDiff < 0) {
       return "La date cible est dans le futur";
-    } else if (timeDiff < 86400000) { // Si moins d'un jour (86400000 ms = 1 jour)
-      var hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60)); // Convertir en heures
+    } else if (timeDiff < 86400000) {
+      var hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60));
       return hoursDiff + " heure(s)";
     } else {
-      var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24)); // Convertir en jours
+      var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
       return daysDiff + " jour(s)";
     }
   }
   
-  // Exemple d'utilisation
-  var date = "2023-06-08 10:58:27 +0000 UTC";
-  var result = calculateDaysSince(date);
-  console.log(result);
-  
+
   
   
   
